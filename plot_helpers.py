5194b71e5ee0ac09e275ed19f77a360e7f191660
Robert P. Goldman <rpgoldman@sift.net>
2022-06-10 21:27:29 -0500

    Make better plots for gating.

--- plot_helpers.py
+++ plot_helpers.py
@@ -3,7 +3,8 @@ import seaborn as sns
 import matplotlib as mpl
 import numpy as np
 
-from typing import Optional
+from typing import Optional, Tuple, Union
+
 
 def point_cloud_to_histogram(df, logscale: bool = True):
     """
@@ -38,7 +39,8 @@ def point_cloud_to_histogram(df, logscale: bool = True):
         bins = np.array([4 + 0.02 * x for x in range(0, 101)])
     else:
         tmp = df.copy()
-        bins = np.array([-100_000 + 10_000 * x for x in range(0, 111)])
+        # -100_000 to 1_100_000
+        bins = np.array([-100_000 + 10_000 * x for x in range(0, 121)])
         
     tmp = tmp[['FSC_A', 'SSC_A']]
     tmp.loc[:, 'FSC_A_bins'] = pd.cut(tmp['FSC_A'], bins=bins)
@@ -85,11 +87,26 @@ def make_heatmap(histo: pd.DataFrame, logscale: bool, ax: Optional[mpl.axes.Axes
         ticklabels = [f"{x:.1f}" for x in [4 + 0.2 * x for x in range(0, 11)]]
         ticks = np.array([10 * x for x in range(0, 11)])
     else:
-        ticklabels = ["%d"%(100000 * x -100_000) for x in range(0, 12)]
-        ticks = np.array([10 * x for x in range(0, 12)])
+        ticklabels = ["%d"%(100_000 * x -100_000) for x in range(0, 13)]
+        ticks = np.array([10 * x for x in range(0, 13)])
     ax.set_xticks(ticks)
     ax.set_yticks(ticks)
     ax.set_xticklabels(ticklabels)
     ticklabels.reverse()
     ax.set_yticklabels(ticklabels)
     return ax
+
+def translate(x: Union[float, Tuple[float, float]], y:Optional[float] = None, logscale: bool = False) -> Tuple[float, float]:
+    if y is None:
+        assert isinstance(x, tuple) and len(x) == 2
+        y = x[1]
+        x = x[0]
+    if logscale:
+        raise NotImplementedError("Need to add heatmap translation for logscale plots")
+    # x does not need origin correction
+    xcoord = x + 100_000 # origin is -100_000
+    # scale
+    xcoord = xcoord / 10_000
+    ycoord = 1_100_000 - y
+    ycoord = ycoord / 10_000
+    return xcoord, ycoord
